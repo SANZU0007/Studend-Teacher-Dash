d60059c39f0e675a9f5c9e10d4346f0d663f29ae
sanjay <LENOVO@DESKTOP-AKPL9L4.(none)>
2023-09-02 01:49:16 +0530

    last committt

--- src/Componends/Editstudents.js
+++ src/Componends/Editstudents.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Base from "../Base/Base";
-import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
-const Editstudents = ({ studId, students,setStudents }) => {
+const Editstudents = ({ students, setStudents }) => {
+  const { id } = useParams();
 
- 
-  const {id} = useParams();
-  
   const [idx, setIdx] = useState("");
   const [name, setName] = useState("");
   const [batch, setBatch] = useState("");
@@ -23,67 +21,63 @@ const Editstudents = ({ studId, students,setStudents }) => {
       setGender(studentData.gender);
       setEducation(studentData.education);
     }
-  }, [studId, students]);
+  }, [id, students]);
 
-  const updateStudent = ()=>{
-    const studentIndex = students.findIndex((stud)=>stud.id === id)
-  const updatedStudent = {
-    id,
-    name,
-    batch,
-    gender,
-    education
-  }
-  students[studentIndex] = updatedStudent
-  setStudents([...students])
-  navigate("/students")
-}
+  const updateStudent = () => {
+    const updatedStudent = {
+      id: idx,
+      name,
+      batch,
+      gender,
+      education,
+    };
+
+    const updatedStudents = students.map((stud) =>
+      stud.id === id ? updatedStudent : stud
+    );
+
+    setStudents(updatedStudents);
+    navigate("/students");
+  };
 
   return (
-    
+    <Base title={"Fill in to edit the student"} description={"Edit student info"}>
+      <div className="form-group">
+        <h4>Update</h4>
 
-    <Base
-    title ={"fil in to edit the student "}
-    description ={"Edit student info"}>
-
-    
-    <div className="form-group">
-      <h4>update</h4>
-
-      <input
-        placeholder="enter the id of student"
-        type="number"
-        value={idx}
-        onChange={(e) => setIdx(e.target.value)}
-      />
-      <input
-        placeholder="enter the name  of student"
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <input
-        placeholder="enter the batch of student"
-        type="text"
-        value={batch}
-        onChange={(e) => setBatch(e.target.value)}
-      />
-      <input
-        placeholder="enter the gender of student"
-        type="text"
-        value={gender}
-        onChange={(e) => setGender(e.target.value)}
-      />
-      <input
-        placeholder="enter the education of student"
-        type="text"
-        value={education}
-        onChange={(e) => setEducation(e.target.value)}
-      />
-      <button onClick={updateStudent}>updatedStudent</button>
-    </div>
+        <input
+          placeholder="Enter the ID of the student"
+          type="number"
+          value={idx}
+          onChange={(e) => setIdx(e.target.value)}
+        />
+        <input
+          placeholder="Enter the name of the student"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          placeholder="Enter the batch of the student"
+          type="text"
+          value={batch}
+          onChange={(e) => setBatch(e.target.value)}
+        />
+        <input
+          placeholder="Enter the gender of the student"
+          type="text"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+        />
+        <input
+          placeholder="Enter the education of the student"
+          type="text"
+          value={education}
+          onChange={(e) => setEducation(e.target.value)}
+        />
+        <button onClick={updateStudent}>Update Student</button>
+      </div>
     </Base>
-   
   );
 };
 
